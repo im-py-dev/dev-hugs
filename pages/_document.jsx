8cf880cd99871748580edf0f84e140e11ea57212
Ali Mughrabi <impydev@gmail.com>
2023-05-23 20:50:20 -0700

    Fix It

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -56,8 +56,15 @@ export default class MyDocument extends Document {
           />
          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="impydev" data-description="Support me on Buy me a coffee!" data-message="Buy Me a Coffee? 🤔" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"/>
 
-		<script src="../src/components/KoFiWidget.js" />
-
+		<script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"></script>
+		<script>
+		  kofiWidgetOverlay.draw('impydev', {
+			'type': 'floating-chat',
+			'floating-chat.donateButton.text': 'Support me',
+			'floating-chat.donateButton.background-color': '#d9534f',
+			'floating-chat.donateButton.text-color': '#fff'
+		  });
+		</script>
 
           {this.props.styleTags}
         </Head>
